b80dc1eae8f262d7b87f468f0f3e17caa4022e2f
allankennethrosal <rosal.allankenneth20@gmail.com>
2021-09-10 23:34:10 +0800

    Added questionnaire items for logical-math and spatial pages

--- src/data/assessmentItems.js
+++ src/data/assessmentItems.js
@@ -95,4 +95,183 @@ const linguisticItems = {
   ]
 };
 
-module.exports = { linguisticItems };
+const logicalMathItems = {
+  questions: [
+    {
+      type: itemType,
+      name: "B1",
+      title: "I can easily compute numbers in my head.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "B2",
+      title: "Math and/or science were among my favorite subjects in school.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "B3",
+      title:
+        "I enjoy playing games or solving brainteasers that require logical thinking.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "B4",
+      title:
+        "I like to set up little “what if” experiments (for example, “What if I double the amount of water I give to my rosebush each week?”).",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "B5",
+      title:
+        "My mind searches for patterns, regularities, or logical sequences in things.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "B6",
+      title: "I’m interested in new developments in science.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "B7",
+      title: "I believe that almost everything has a rational explanation.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "B8",
+      title:
+        "I sometimes think in clear, abstract, wordless, imageless concepts.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "B9",
+      title:
+        "I like finding logical flaws in things that people say and do at home and work.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "B10",
+      title:
+        " I feel more comfortable when something has been measured, categorized, analyzed, or quantifi ed in some way.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    }
+  ]
+};
+
+const spatialItems = {
+  questions: [
+    {
+      type: itemType,
+      name: "C1",
+      title: "I often see clear visual images when I close my eyes.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "C2",
+      title: "I’m sensitive to color.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "C3",
+      title:
+        "I frequently use a camera or camcorder to record what I see around me.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "C4",
+      title: "I enjoy doing jigsaw puzzles, mazes, and other visual puzzles.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "C5",
+      title: "I have vivid dreams at night.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "C6",
+      title: "I can generally fi nd my way around unfamiliar territory.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "C7",
+      title: "I like to draw or doodle.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "C8",
+      title: "Geometry was easier for me than algebra in schooL.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "C9",
+      title:
+        "I can comfortably imagine how something might appear if it were looked down on from directly above in a bird’s-eye view.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    },
+    {
+      type: itemType,
+      name: "C10",
+      title:
+        "I prefer looking at reading material that is heavily illustrated.",
+      isRequired: true,
+      mininumRateDescription: minRate,
+      maximumRateDescription: maxRate
+    }
+  ]
+};
+
+module.exports = { linguisticItems, logicalMathItems, spatialItems };
